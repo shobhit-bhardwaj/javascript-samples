599aa6d4143e9df97da3f803d45aaa63299f0dd9
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-11-03 20:23:34 +0530

    Commit package.json

--- modules/index.js
+++ modules/index.js
@@ -1,11 +1,12 @@
-//  Default ->  import ... from '';
-//  Named   ->  import { ... } from '';
-//  Mixed   ->  import ..., { ... } from '';
+//  Default     ->  import ... from '';
+//  Named       ->  import { ... } from '';
+//  Mixed       ->  import ..., { ... } from '';
+//  Import *    ->  import * as alias from ''
 
-import Person from './person.js';
-import { addition, subtraction, PI } from './calculator.js';
+import Person from "./person.js";
+import { addition, subtraction, PI } from "./calculator.js";
 
-const person = new Person('Shobhit Bhardwaj', 30);
+const person = new Person("Shobhit Bhardwaj", 30);
 person.showInfo();
 
 let add = addition(12, 5);
